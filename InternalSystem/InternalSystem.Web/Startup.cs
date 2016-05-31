04f7f97c90d71827005d3ee7447371f671a2b5af
haojun.sun <coolboss1@sina.com>
2016-05-31 15:17:15 +0800

    创建

---
+++ InternalSystem/InternalSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternalSystem.Web.Startup))]
+namespace InternalSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
