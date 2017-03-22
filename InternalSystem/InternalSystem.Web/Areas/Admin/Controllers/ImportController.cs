8c00a97e2b92e39209b5301474fcc3fd2c11e5da
gsboston <549013661@qq.com>
2017-03-22 15:03:30 +0800

    前端样式修改，图片更换

--- InternalSystem/InternalSystem.Web/Areas/Admin/Controllers/ImportController.cs
+++ InternalSystem/InternalSystem.Web/Areas/Admin/Controllers/ImportController.cs
@@ -52,183 +52,183 @@ namespace InternalSystem.Web.Areas.Admin.Controllers
             //StreamReader sr = new StreamReader(path + file, System.Text.Encoding.Default);
             //string data = sr.ReadToEnd();
             //sr.Close();
-            #region 解析 文件 导入 数据 _worldHeritageService
+            //#region 解析 文件 导入 数据 _worldHeritageService
 
-            //解析文件 读取文件 导出datatable
-            DirectoryInfo di = new DirectoryInfo(path + "\\fyexcel");
-            DirectoryInfo[] dir = di.GetDirectories();//获取子文件夹列表
-            var wh = new List<WorldHeritage>();
+            ////解析文件 读取文件 导出datatable
+            //DirectoryInfo di = new DirectoryInfo(path + "\\fyexcel");
+            //DirectoryInfo[] dir = di.GetDirectories();//获取子文件夹列表
+            //var wh = new List<WorldHeritage>();
 
-            foreach (FileInfo file in di.GetFiles())
-            {
-                Console.WriteLine(file.Name);
-                //ExcelHelper eh = new ExcelHelper("C:\\Users\\Administrator\\Desktop\\fyexcel\\" + file.Name);
-                DataTable dt = new DataTable();
-                dt = ExcelToDataTable(path + "\\fyexcel\\" + file.Name);
-                //Console.WriteLine(dt.Rows.count);
+            //foreach (FileInfo file in di.GetFiles())
+            //{
+            //    Console.WriteLine(file.Name);
+            //    //ExcelHelper eh = new ExcelHelper("C:\\Users\\Administrator\\Desktop\\fyexcel\\" + file.Name);
+            //    DataTable dt = new DataTable();
+            //    dt = ExcelToDataTable(path + "\\fyexcel\\" + file.Name);
+            //    //Console.WriteLine(dt.Rows.count);
 
-                for (int i = 0; i < dt.Rows.Count; i++)
-                {
-                    var wh1 = new WorldHeritage();
-                    for (int j = 0; j < dt.Columns.Count; j++)
-                    {
-                        if (j == 0)
-                            wh1.InventoryId = dt.Rows[i][j].ToString();
-                        if (j == 1)
-                        {
-                            wh1.ArtificialId = dt.Rows[i][j].ToString();
-                            wh1.FileName = "~/Uploads/importVideo/" + dt.Rows[i][j].ToString() + ".mp4";
-                        }
-                        if (j == 2)
-                            wh1.TitleProper = dt.Rows[i][j].ToString();
-                        if (j == 3)
-                            wh1.Title = dt.Rows[i][j].ToString();
-                        if (j == 4)
-                            wh1.FirstLevel = dt.Rows[i][j].ToString();
-                        if (j == 5)
-                            wh1.SecondLevel = dt.Rows[i][j].ToString();
-                        if (j == 6)
-                            wh1.ProjectCode = dt.Rows[i][j].ToString();
-                        if (j == 7)
-                            wh1.SubItemNumber = dt.Rows[i][j].ToString();
-                        if (j == 8)
-                            wh1.Batch = dt.Rows[i][j].ToString();
-                        if (j == 9)
-                            wh1.Notes = dt.Rows[i][j].ToString();
-                        if (j == 10)
-                            wh1.ApplicationTime = dt.Rows[i][j].ToString();
-                        if (j == 11)
-                            wh1.Nation = dt.Rows[i][j].ToString();
-                        if (j == 12)
-                            wh1.NationalBranch = dt.Rows[i][j].ToString();
-                        if (j == 13)
-                            wh1.Gender = dt.Rows[i][j].ToString();
-                        if (j == 14)
-                            wh1.Birth = dt.Rows[i][j].ToString();
-                        if (j == 15)
-                            wh1.Education = dt.Rows[i][j].ToString();
-                        if (j == 16)
-                            wh1.Occupation = dt.Rows[i][j].ToString();
-                        if (j == 17)
-                            wh1.Successor = dt.Rows[i][j].ToString();
-                        if (j == 19)
-                            wh1.Genre = dt.Rows[i][j].ToString();
-                        if (j == 20)
-                        {
-                            if (dt.Rows[i][j].ToString() == "")
-                                wh1.Municipalities = "不清";
-                            else
-                                wh1.Municipalities = dt.Rows[i][j].ToString();
-                        }
-                        if (j == 21)
-                        {
-                            if (dt.Rows[i][j].ToString() == "")
-                                wh1.Region = "不清";
-                            else
-                                wh1.Region = dt.Rows[i][j].ToString();
-                        }
-                        if (j == 22)
-                        {
-                            if (dt.Rows[i][j].ToString() == "")
-                                wh1.CountyLevelCity = "不清";
-                            else
-                                wh1.CountyLevelCity = dt.Rows[i][j].ToString();
-                        }
-                        if (j == 23)
-                        {
-                            if (dt.Rows[i][j].ToString() == "")
-                                wh1.Township = "不清";
-                            else
-                                wh1.Township = dt.Rows[i][j].ToString();
-                        }
-                        if (j == 24)
-                            wh1.Village = dt.Rows[i][j].ToString();
-                        if (j == 25)
-                            wh1.DeclarationArea = dt.Rows[i][j].ToString();
-                        if (j == 26)
-                            wh1.ProtectionUnit = dt.Rows[i][j].ToString();
-                        if (j == 27)
-                            wh1.EcologicalZoneProject = dt.Rows[i][j].ToString();
-                        if (j == 28)
-                            wh1.ProductiveProtectionBase = dt.Rows[i][j].ToString();
-                        if (j == 29)
-                            wh1.ProvinceCode = dt.Rows[i][j].ToString();
-                        if (j == 30)
-                            wh1.PostTime = dt.Rows[i][j].ToString();
-                        if (j == 31)
-                            wh1.Death = dt.Rows[i][j].ToString();
-                        if (j == 32)
-                            wh1.Adopt = dt.Rows[i][j].ToString();
-                        if (j == 33)
-                            wh1.Language = dt.Rows[i][j].ToString();
-                        if (j == 34)
-                            wh1.CreatorName = dt.Rows[i][j].ToString();
-                        if (j == 35)
-                            wh1.ResponsibilityCreator = dt.Rows[i][j].ToString();
-                        if (j == 36)
-                            wh1.DataFormat = dt.Rows[i][j].ToString();
-                        if (j == 37)
-                            wh1.DataProvider = dt.Rows[i][j].ToString();
-                        if (j == 38)
-                            wh1.CollectionUnit = dt.Rows[i][j].ToString();
-                        if (j == 39)
-                            wh1.DigitalFormat = dt.Rows[i][j].ToString();
-                        if (j == 40)
-                            wh1.Size = dt.Rows[i][j].ToString();
-                        if (j == 41)
-                            wh1.Duration = dt.Rows[i][j].ToString();
-                        if (j == 42)
-                            wh1.ResolvingPower = dt.Rows[i][j].ToString();
-                        if (j == 43)
-                            wh1.KBps = dt.Rows[i][j].ToString();
-                        if (j == 44)
-                            wh1.SamplingFrequency = dt.Rows[i][j].ToString();
-                        if (j == 45)
-                            wh1.Channels = dt.Rows[i][j].ToString();
-                        if (j == 46)
-                            wh1.StoragePlace = dt.Rows[i][j].ToString();
-                        if (j == 47)
-                            wh1.DisplayLevel = dt.Rows[i][j].ToString();
-                        if (j == 48)
-                            wh1.Description = dt.Rows[i][j].ToString();
-                        if (j == 49)
-                        {
-                            string aa = dt.Rows[i][j].ToString(), bb = "";
-                            Regex obj = new Regex(@"[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}");
-                            if (obj.IsMatch(aa))
-                            {
-                                bb = "20" + aa.Split('/')[2] + "-" + aa.Split('/')[0] + "-" + aa.Split('/')[1];
-                            }
-                            wh1.DescriptionTime = bb;//dt.Rows[i][j].ToString();
-                        }
-                        //if (j == 50)
-                        //    wh1.Audit = dt.Rows[i][j].ToString();
-                        //if (j == 51)
-                        //    wh1.AuditTime = dt.Rows[i][j].ToString();
-                        if (j == 52)
-                            wh1.Organization = dt.Rows[i][j].ToString();
-                        if (j == 53)
-                            wh1.Remarks = dt.Rows[i][j].ToString();
-                        if (j == 54)
-                            wh1.url = dt.Rows[i][j].ToString();
+            //    for (int i = 0; i < dt.Rows.Count; i++)
+            //    {
+            //        var wh1 = new WorldHeritage();
+            //        for (int j = 0; j < dt.Columns.Count; j++)
+            //        {
+            //            if (j == 0)
+            //                wh1.InventoryId = dt.Rows[i][j].ToString();
+            //            if (j == 1)
+            //            {
+            //                wh1.ArtificialId = dt.Rows[i][j].ToString();
+            //                wh1.FileName = "~/Uploads/importVideo/" + dt.Rows[i][j].ToString() + ".mp4";
+            //            }
+            //            if (j == 2)
+            //                wh1.TitleProper = dt.Rows[i][j].ToString();
+            //            if (j == 3)
+            //                wh1.Title = dt.Rows[i][j].ToString();
+            //            if (j == 4)
+            //                wh1.FirstLevel = dt.Rows[i][j].ToString();
+            //            if (j == 5)
+            //                wh1.SecondLevel = dt.Rows[i][j].ToString();
+            //            if (j == 6)
+            //                wh1.ProjectCode = dt.Rows[i][j].ToString();
+            //            if (j == 7)
+            //                wh1.SubItemNumber = dt.Rows[i][j].ToString();
+            //            if (j == 8)
+            //                wh1.Batch = dt.Rows[i][j].ToString();
+            //            if (j == 9)
+            //                wh1.Notes = dt.Rows[i][j].ToString();
+            //            if (j == 10)
+            //                wh1.ApplicationTime = dt.Rows[i][j].ToString();
+            //            if (j == 11)
+            //                wh1.Nation = dt.Rows[i][j].ToString();
+            //            if (j == 12)
+            //                wh1.NationalBranch = dt.Rows[i][j].ToString();
+            //            if (j == 13)
+            //                wh1.Gender = dt.Rows[i][j].ToString();
+            //            if (j == 14)
+            //                wh1.Birth = dt.Rows[i][j].ToString();
+            //            if (j == 15)
+            //                wh1.Education = dt.Rows[i][j].ToString();
+            //            if (j == 16)
+            //                wh1.Occupation = dt.Rows[i][j].ToString();
+            //            if (j == 17)
+            //                wh1.Successor = dt.Rows[i][j].ToString();
+            //            if (j == 19)
+            //                wh1.Genre = dt.Rows[i][j].ToString();
+            //            if (j == 20)
+            //            {
+            //                if (dt.Rows[i][j].ToString() == "")
+            //                    wh1.Municipalities = "不清";
+            //                else
+            //                    wh1.Municipalities = dt.Rows[i][j].ToString();
+            //            }
+            //            if (j == 21)
+            //            {
+            //                if (dt.Rows[i][j].ToString() == "")
+            //                    wh1.Region = "不清";
+            //                else
+            //                    wh1.Region = dt.Rows[i][j].ToString();
+            //            }
+            //            if (j == 22)
+            //            {
+            //                if (dt.Rows[i][j].ToString() == "")
+            //                    wh1.CountyLevelCity = "不清";
+            //                else
+            //                    wh1.CountyLevelCity = dt.Rows[i][j].ToString();
+            //            }
+            //            if (j == 23)
+            //            {
+            //                if (dt.Rows[i][j].ToString() == "")
+            //                    wh1.Township = "不清";
+            //                else
+            //                    wh1.Township = dt.Rows[i][j].ToString();
+            //            }
+            //            if (j == 24)
+            //                wh1.Village = dt.Rows[i][j].ToString();
+            //            if (j == 25)
+            //                wh1.DeclarationArea = dt.Rows[i][j].ToString();
+            //            if (j == 26)
+            //                wh1.ProtectionUnit = dt.Rows[i][j].ToString();
+            //            if (j == 27)
+            //                wh1.EcologicalZoneProject = dt.Rows[i][j].ToString();
+            //            if (j == 28)
+            //                wh1.ProductiveProtectionBase = dt.Rows[i][j].ToString();
+            //            if (j == 29)
+            //                wh1.ProvinceCode = dt.Rows[i][j].ToString();
+            //            if (j == 30)
+            //                wh1.PostTime = dt.Rows[i][j].ToString();
+            //            if (j == 31)
+            //                wh1.Death = dt.Rows[i][j].ToString();
+            //            if (j == 32)
+            //                wh1.Adopt = dt.Rows[i][j].ToString();
+            //            if (j == 33)
+            //                wh1.Language = dt.Rows[i][j].ToString();
+            //            if (j == 34)
+            //                wh1.CreatorName = dt.Rows[i][j].ToString();
+            //            if (j == 35)
+            //                wh1.ResponsibilityCreator = dt.Rows[i][j].ToString();
+            //            if (j == 36)
+            //                wh1.DataFormat = dt.Rows[i][j].ToString();
+            //            if (j == 37)
+            //                wh1.DataProvider = dt.Rows[i][j].ToString();
+            //            if (j == 38)
+            //                wh1.CollectionUnit = dt.Rows[i][j].ToString();
+            //            if (j == 39)
+            //                wh1.DigitalFormat = dt.Rows[i][j].ToString();
+            //            if (j == 40)
+            //                wh1.Size = dt.Rows[i][j].ToString();
+            //            if (j == 41)
+            //                wh1.Duration = dt.Rows[i][j].ToString();
+            //            if (j == 42)
+            //                wh1.ResolvingPower = dt.Rows[i][j].ToString();
+            //            if (j == 43)
+            //                wh1.KBps = dt.Rows[i][j].ToString();
+            //            if (j == 44)
+            //                wh1.SamplingFrequency = dt.Rows[i][j].ToString();
+            //            if (j == 45)
+            //                wh1.Channels = dt.Rows[i][j].ToString();
+            //            if (j == 46)
+            //                wh1.StoragePlace = dt.Rows[i][j].ToString();
+            //            if (j == 47)
+            //                wh1.DisplayLevel = dt.Rows[i][j].ToString();
+            //            if (j == 48)
+            //                wh1.Description = dt.Rows[i][j].ToString();
+            //            if (j == 49)
+            //            {
+            //                string aa = dt.Rows[i][j].ToString(), bb = "";
+            //                Regex obj = new Regex(@"[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}");
+            //                if (obj.IsMatch(aa))
+            //                {
+            //                    bb = "20" + aa.Split('/')[2] + "-" + aa.Split('/')[0] + "-" + aa.Split('/')[1];
+            //                }
+            //                wh1.DescriptionTime = bb;//dt.Rows[i][j].ToString();
+            //            }
+            //            //if (j == 50)
+            //            //    wh1.Audit = dt.Rows[i][j].ToString();
+            //            //if (j == 51)
+            //            //    wh1.AuditTime = dt.Rows[i][j].ToString();
+            //            if (j == 52)
+            //                wh1.Organization = dt.Rows[i][j].ToString();
+            //            if (j == 53)
+            //                wh1.Remarks = dt.Rows[i][j].ToString();
+            //            if (j == 54)
+            //                wh1.url = dt.Rows[i][j].ToString();
 
 
-                    }
-                    wh1.User = _managerService.Get(2);
+            //        }
+            //        wh1.User = _managerService.Get(2);
 
-                    wh1.CreatedUtc = DateTime.Now;
-                    wh1.Description = "admin";
-                    wh1.DescriptionTime = DateTime.Now.ToString();
-                    wh1.HeritageType = 0;
-                    wh1.IsEffect = 1;
-                    wh.Add(wh1);
-                }
-                //eh.DataTableToExcel(dt, true, @"C:\Users\Administrator\Desktop\模板.xls", pro + "\\" + file.Name, pro);
-            }
+            //        wh1.CreatedUtc = DateTime.Now;
+            //        wh1.Description = "admin";
+            //        wh1.DescriptionTime = DateTime.Now.ToString();
+            //        wh1.HeritageType = 0;
+            //        wh1.IsEffect = 1;
+            //        wh.Add(wh1);
+            //    }
+            //    //eh.DataTableToExcel(dt, true, @"C:\Users\Administrator\Desktop\模板.xls", pro + "\\" + file.Name, pro);
+            //}
 
-            _worldHeritageService.Import(wh);//一次性 录入数据 没测试 导入的时候你测试一下
+            //_worldHeritageService.Import(wh);//一次性 录入数据 没测试 导入的时候你测试一下
 
-            #endregion
+            //#endregion
             return RedirectToAction("List");
         }
         private IWorkbook workbook = null;
